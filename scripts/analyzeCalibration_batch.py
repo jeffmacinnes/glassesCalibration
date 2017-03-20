63af136267b3df8b7b25b7ffe261a6707ae026c0
Jeff MacInnes <jeff.macinnes@gmail.com>
2017-03-20 16:31:05 -0400

    added batch submission script for analyzeCalibration

---
+++ scripts/analyzeCalibration_batch.py
@@ -0,0 +1,34 @@
+"""
+Batch submit multiple subjects to the analyzeCalibration script
+"""
+
+import os
+from os.path import join
+import pandas as pd 
+
+conditions = ['101_Tobii_2M_0deg',
+			'101_Tobii_2M_10Ldeg',
+			'101_Tobii_2M_10Rdeg',
+			'101_Tobii_3M_0deg',
+			'101_Tobii_3M_10Ldeg',
+			'101_Tobii_3M_10Rdeg']
+
+conditions = ['101_Tobii_2M_0deg',
+			'101_Tobii_2M_10Ldeg']
+
+# load the metadata table
+metadata_df = pd.read_table(join('../data', 'metadataTable.txt'), sep='\t', header=0)
+
+# create new column that concatenates subfields to match condition formatting
+metadata_df['condition'] = metadata_df['Subj'].map(str) + '_' + metadata_df['Glasses'] + '_' + metadata_df['Distance'] + '_' + metadata_df['Offset']
+
+
+for cond in conditions:
+	print('Submitting job for: {}'.format(cond))
+
+	try:
+		cmd_str = 'python analyzeCalibration.py ' + cond 
+		os.system(cmd_str)
+	except:
+		print('FAILED TO RUN:  {}'.format(cond))
+
