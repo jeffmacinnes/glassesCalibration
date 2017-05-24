370a0c5bce02a2d8dedb1725ca186e3d198ec9f7
Jeff MacInnes <jeff.macinnes@gmail.com>
2017-05-24 19:12:14 -0400

    getFPS finished

--- scripts/getFPS.py
+++ scripts/getFPS.py
@@ -9,15 +9,39 @@ import os
 from os.path import join
 import cv2
 import numpy as np
-import pandas as import pd
+import pandas as pd
 
 # get the opencv version
 OPENCV3 = (cv2.__version__.split('.')[0] == '3')
 print("OPENCV version " + cv2.__version__)
+
+# output
+outputFile = open(join('../analysis', 'fps.txt'), 'w')
 
 # walk through sub directories in the data dir
 data_dir = '../data'
 for f in os.listdir(data_dir):
     # only look in subject data directories
     if f[:3] in ['101', '102', '103']:
-        print f
+
+        # look for worldCamera.mp4
+        try:
+            vidFile = join(data_dir, f, 'worldCamera.mp4')
+            vid = cv2.VideoCapture(vidFile)
+
+            # retrieve the fps
+            if OPENCV3:
+                fps = vid.get(cv2.CAP_PROP_FPS)
+            else:
+                fps = vid.get(cv2.cv.CV_CAP_PROP_FPS)
+
+            # write fps for this condition
+            outputFile.write('{}\t{}\n'.format(f, fps))
+
+            # close the video
+            vid.release()
+        except:
+            print('No video file found for {}'.format(f))
+
+# close output
+outputFile.close()
